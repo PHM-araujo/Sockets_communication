9a7f46ca356705f72bcc0b931f9bdfc47dd66bd3
Arthur Nina <63884171+arthurhn@users.noreply.github.com>
2021-09-18 11:30:28 -0300

    Add files via upload

---
+++ socket/Client/include/Cliente.h
@@ -0,0 +1,24 @@
+#ifndef CLIENTE_H
+#define CLIENTE_H
+
+#include "SocketClient.h"
+
+class Cliente : public SocketClient{
+
+    public:
+        enum Beverage{Agua = 0, Refri, Cerva};
+
+    private:
+
+        int quant_agua, quant_refri, quant_cerva;
+
+    public:
+        Cliente(int port, std::string IP);
+        void routine();
+        std::string requestBeverage();
+        void validaInteger(int& in, std::string out);
+        void validaInput(int& in, std::string out, int val_max);
+
+};
+
+#endif
---
+++ socket/Server/src/main.cpp
@@ -0,0 +1,9 @@
+#include "Loja.h"
+
+int main(){
+    Loja loja(8080);
+
+    loja.routine();
+
+    return 0;
+}
